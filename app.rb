13c77fa67cd98084799d3b3103bf2a695886c97f
BigBeardy <westderick70@gmail.com>
2017-08-25 09:29:26 -0400

    closer

--- app.rb
+++ app.rb
@@ -13,12 +13,24 @@ post '/toppings' do
 	session[:sauce] = params[:sauce]
 	session[:crust] = params[:crust]
  redirect '/confirm'
- 
- # p "#{toppings.join(', ')}"
-end
+ end
+
 get '/confirm' do
   erb :confirm, locals: {toppings: session[:toppings], veg: session[:veg], meat: session[:meat], cheese: session[:cheese], sauce: session[:sauce], crust: session[:crust]}
 end
 post '/confirm' do
-  "Hello World"
+	session[:toppings] = params[:toppings]
+	session[:meat] = params[:meat]
+	session[:veg] = params[:veg]
+	session[:cheese] = params[:cheese]
+	session[:sauce] = params[:sauce]
+	session[:crust] = params[:crust]
+	redirect '/final' do 
+end
+
+  get '/final' do 
+	erb :final, locals: {final: session[:confirmchoices]}
+	erb :confirm, locals: {toppings: session[:toppings], veg: session[:veg], meat: session[:meat], cheese: session[:cheese], sauce: session[:sauce], crust: session[:crust]}
+
+end
 end
