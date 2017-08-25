b0ce55124bda903151a26f003c8595115defbc06
BigBeardy <westderick70@gmail.com>
2017-08-24 22:18:33 -0400

    completed confirm page

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require "sinatra"
 
-get '/' do 
-"hello world"
+enable :sessions
+get '/' do
+  erb :index
+end
+   
+post '/toppings' do 
+	session[:toppings] = params[:toppings]
+	session[:meat] = params[:meat]
+	session[:veg] = params[:veg]
+	session[:cheese] = params[:cheese]
+	session[:sauce] = params[:sauce]
+	session[:crust] = params[:crust]
+ redirect '/confirm'
+ 
+ # p "#{toppings.join(', ')}"
+end
+get '/confirm' do
+  erb :confirm, locals: {toppings: session[:toppings], veg: session[:veg], meat: session[:meat], cheese: session[:cheese], sauce: session[:sauce], crust: session[:crust]}
+end
+post '/confirm' do
+  "Hello World"
 end
