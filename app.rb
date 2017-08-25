efd13bf1d224a7c4ffae73e5d9a3d55d1453a0f7
BigBeardy <westderick70@gmail.com>
2017-08-25 12:25:04 -0400

    got final page up

--- app.rb
+++ app.rb
@@ -19,18 +19,25 @@ get '/confirm' do
   erb :confirm, locals: {toppings: session[:toppings], veg: session[:veg], meat: session[:meat], cheese: session[:cheese], sauce: session[:sauce], crust: session[:crust]}
 end
 post '/confirm' do
+	session[:confirmed] = params[:confirmed]
 	session[:toppings] = params[:toppings]
 	session[:meat] = params[:meat]
 	session[:veg] = params[:veg]
 	session[:cheese] = params[:cheese]
 	session[:sauce] = params[:sauce]
 	session[:crust] = params[:crust]
-	redirect '/final' do 
+	redirect '/final'
+end 
+
+get '/final' do 
+	erb :final, locals: {confirmed: session[:confirmed],toppings: session[:toppings], veg: session[:veg], meat: session[:meat], cheese: session[:cheese], sauce: session[:sauce], crust: session[:crust]}
+end
+post '/final' do
+# session[:toppings] = params[:toppings]
+# 	session[:meat] = params[:meat]
+# 	session[:veg] = params[:veg]
+# 	session[:cheese] = params[:cheese]
+# 	session[:sauce] = params[:sauce]
+# 	session[:crust] = params[:crust] 
 end
 
-  get '/final' do 
-	erb :final, locals: {final: session[:confirmchoices]}
-	erb :confirm, locals: {toppings: session[:toppings], veg: session[:veg], meat: session[:meat], cheese: session[:cheese], sauce: session[:sauce], crust: session[:crust]}
-
-end
-end
